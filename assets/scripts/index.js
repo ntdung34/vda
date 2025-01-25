edccdbc5f430bf6aa30f9811430018f84a2dd39d
Dung <daobaobi75@gmail.com>
2025-01-25 20:40:20 +0700

    modify

---
+++ assets/scripts/index.js
@@ -0,0 +1,129 @@
+
+
+
+window.onload = function() {
+
+    checkOS();
+    initializeSwipers();
+
+}
+
+function checkOS() {
+
+    const userAgent = navigator.userAgent || window.opera;
+    if( /android/i.test( userAgent ) ) { document.body.classList.add( "android" ); }
+
+}
+
+function initializeSwipers() {
+
+    const
+        s1 = new Swiper( ".mySwiper1", {
+            // centeredSlide: true,
+            slidesPerView: 1,
+
+            // Optional parameters
+            direction: "horizontal",
+            loop: true,
+            breakpoints: {
+                1699.98: {
+                    slidesPerView: 4
+                },
+                1269.98: {
+                    slidesPerView: 3
+                },
+                499.98: {
+                    slidesPerView: 2
+                }
+            },
+
+            // Pagination
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            // Scrollbar
+            scrollbar: {
+                el: ".swiper-scrollbar",
+            },
+        } ),
+        s3 = new Swiper( ".mySwiper3", {
+            // centeredSlide: true,
+            slidesPerView: 1,
+
+            // Optional parameters
+            direction: "horizontal",
+            loop: true,
+            breakpoints: {
+                // 1749.98: {
+                //     slidesPerView: 3
+                // },
+                1269.98: {
+                    slidesPerView: 3
+                },
+                499.98: {
+                    slidesPerView: 2
+                }
+            },
+
+            // Pagination
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            // Scrollbar
+            scrollbar: {
+                el: ".swiper-scrollbar",
+            },
+        } ),
+        s4 = new Swiper( ".mySwiper4", {
+            // centeredSlide: true,
+            slidesPerView: 1,
+
+            // Optional parameters
+            direction: "horizontal",
+            loop: true,
+            breakpoints: {
+                1699.98: {
+                    slidesPerView: 4
+                },
+                1199.98: {
+                    slidesPerView: 3
+                },
+                499.98: {
+                    slidesPerView: 2
+                }
+            },
+
+            // Pagination
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            // Scrollbar
+            scrollbar: {
+                el: ".swiper-scrollbar",
+            },
+        } );
+
+}
+
+
