7b9c0d9a6c74eaca7f268931000caa6bbf85aa09
Dung <daobaobi75@gmail.com>
2025-01-26 08:45:23 +0700

    modify

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -53,6 +53,41 @@ function initializeSwipers() {
                 el: ".swiper-scrollbar",
             },
         } ),
+        s2 = new Swiper( ".mySwiper2", {
+            // centeredSlide: true,
+            slidesPerView: 1,
+
+            // Optional parameters
+            direction: "horizontal",
+            loop: true,
+            breakpoints: {
+                // 1749.98: {
+                //     slidesPerView: 3
+                // },
+                1269.98: {
+                    slidesPerView: 3
+                },
+                799.98: {
+                    slidesPerView: 2
+                }
+            },
+
+            // Pagination
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            // Scrollbar
+            scrollbar: {
+                el: ".swiper-scrollbar",
+            },
+        } ),
         s3 = new Swiper( ".mySwiper3", {
             // centeredSlide: true,
             slidesPerView: 1,
